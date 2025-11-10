2a4d3ccb94b15c9aebf8d74d57a6b93347d34ace
SimonTheDevloper <simon.b.kurk@gmail.com>
2025-11-10 19:46:55 +0100

    "mini todo app" hinzugefügt bei lesson 5

--- lesson5/server.js
+++ lesson5/server.js
@@ -27,8 +27,41 @@ app.post('/api/user', (req, res) => {
     console.log(`Neuer Benutzer hinzugefügt: ${newUser.name} (ID: ${newUser.id})`);
 });
 
+
+app.post('/api/users', (req, res) => {
+    const { name, email } = req.body
+
+    res.status(201).json(
+        {
+            msg: 'Neuer user erfolgreich erstellt',
+            user: { name, email }
+        }
+    )
+})
 app.get('/api/user', (req, res) => {
     res.status(200).json(user);
 });
 
+// mini To-Do app Projekt
+const todos = [
+    { id: 1, text: 'Essen Kochen' },
+];
+
+app.get('/api/todo', (req, res) => {
+    res.status(200).json(todos);
+});
+
+app.post('/api/todo', (req, res) => { // neue Todo hinzufügen
+
+    if (!req.body.text) {
+        return res.status(400).json({ msg: 'Bitte füge den text hinzu' });
+    }
+    const newTodo = {
+        id: user.length + 1,
+        text: req.body.text
+    }
+
+    todos.push(newTodo)
+    res.status(201).json({ msg: 'Neue Todo wurde erfolgreich hinzugefügt' })
+})
 app.listen(port, () => console.log(`Server läuft auf Port ${port}`));
